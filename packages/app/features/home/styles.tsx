2e1309c57dc85183fab46b43bebeed6a38abe7b3
arthurnog <artuts874@gmail.com>
2023-03-29 09:32:30 -0300

    feat: Rudimentar Search Bar the result is printed on the console for now

---
+++ packages/app/features/home/styles.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      padding: 8,
+    },
+    searchBar: {
+      backgroundColor: '#fff',
+      padding: 8,
+      flexDirection: 'row',
+    },
+    input: {
+      height: 40,
+      backgroundColor: '#eee',
+      paddingHorizontal: 16,
+      borderRadius: 20,
+    },
+  });
+
+export default styles;
