e60740b135f50657e5de94f89668d33a0ad27845
arthurnog <artuts874@gmail.com>
2023-03-31 10:17:20 -0300

    feat: Implementing meaning display

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,15 +1,8 @@
-import { Text, useSx, View, H1, P, Row, A } from 'dripsy'
-import { useCallback, useEffect, useState } from 'react'
-import { TextLink } from 'solito/link'
-import { MotiLink } from 'solito/moti'
+import { View, H1 } from 'dripsy'
+import { useCallback, useState } from 'react'
 import SearchBar from './searchBar'
 import ResultDisplay, { parseResponse, ResultType } from './resultDisplay'
 import styles from './styles';
-
-const mockResults : ResultType[] = [
-  {word:'abacate', phonetic:'/abacate/'},
-  {word:'banana', phonetic:'/banana/'}
-];
 
 export function HomeScreen() {
   const [searchResults, setSearchResults] = useState<ResultType[]>([])
@@ -30,7 +23,13 @@ export function HomeScreen() {
       <H1 sx={{ fontWeight: '800' }}>English Dictionary</H1>
       <View style={styles.container}>
         <SearchBar onButtonPress={handleSearch} />
-        {searchResults.map(result => <ResultDisplay key={result.word} word={result.word} phonetic={result.phonetic} />)}
+        {searchResults.map((result) => (
+          <ResultDisplay
+            key={result.meanings[0]?.definitions[0]?.definition}
+            meanings={result.meanings}
+            word={result.word}
+            phonetic={result.phonetic}
+          />))}
       </View>
       <View sx={{ height: 32 }} />
     </View>
